4f1bb6c8756b5af8902af3f5fdf7fc1e7bd0cc98
Emnajemal <100838789+Emnajemal@users.noreply.github.com>
2022-06-06 14:19:41 +0200

    test

---
+++ src/app/main/apps/ecommerce/addproduct/addproduct.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AddproductComponent } from "app/main/apps/ecommerce/addproduct/addproduct.component";
+
+
+describe('Component: Addproduct', function (){
+
+    let component: AddproductComponent;
+    let fixture: ComponentFixture<AddproductComponent>;
+    beforeEach(() => {
+
+        TestBed.configureTestingModule({
+            declarations: [AddproductComponent],
+            imports: [ReactiveFormsModule, FormsModule],
+        });
+
+        // create component and test fixture
+        fixture = TestBed.createComponent(AddproductComponent);
+
+        // get test component from the fixture
+        component = fixture.componentInstance;
+        component.ngOnInit();
+    });
+
+    it('form invalid when empty', function () {
+        expect(component.addForm.valid).toBeFalsy();
+    });
+
+    it('sellingprice field validity', function (){
+        let sellingprice = component.addForm.controls['sellingprice'];
+        expect(sellingprice.valid).toBeFalsy();
+        sellingprice.setValue(0.00000009)
+        expect(sellingprice.valid).toBeFalsy();
+    });
+});
